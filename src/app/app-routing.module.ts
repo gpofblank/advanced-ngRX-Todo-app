6749f787a97a4a232a4b7e812bee88465a30d805
gpofblank <metodiev.design@gmail.com>
2020-08-19 20:33:46 +0300

    added create users page and did some css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {TodoListPageComponent} from './todo/todo-list-page/todo-list-page.component';
+import {TodoListPageComponent} from './todos/todo-list-page/todo-list-page.component';
 import {AppComponent} from './app.component';
 import {StartComponent} from './start/start.component';
-import {TodoEditPageComponent} from './todo/todo-edit-page/todo-edit-page.component';
+import {TodoEditPageComponent} from './todos/todo-edit-page/todo-edit-page.component';
 
 
 const routes: Routes = [
   {path: '', component: StartComponent},
   {path: 'todos', component: TodoListPageComponent},
-  {path: 'todos/edit/:id', component: TodoEditPageComponent}
+  {path: 'todos/edit/:id', component: TodoEditPageComponent},
+  // {path: 'users', component: U}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TodoListPageComponent} from './todo/todo-list-page/todo-list-page.component';
-import {TodoComponent} from './todo/todo/todo.component';
+import {TodoListPageComponent} from './todos/todo-list-page/todo-list-page.component';
+import {TodoComponent} from './todos/todo/todo.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {StartComponent} from './start/start.component';
@@ -15,14 +15,15 @@ import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {environment} from '../environments/environment';
-import {todoReducerState} from './todo/reducers/todo.reducer';
-import { TodoEditPageComponent } from './todo/todo-edit-page/todo-edit-page.component';
+import {todoReducerState} from './todos/reducers/todo.reducer';
+import { TodoEditPageComponent } from './todos/todo-edit-page/todo-edit-page.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { CreateUserPageComponent } from './users/create-user-page/create-user-page.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     TodoComponent,
     StartComponent,
     TodoEditPageComponent,
+    CreateUserPageComponent,
   ],
   imports: [
     BrowserModule,
