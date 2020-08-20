bcbeac94b2d95ec025be9048f390883ab44278a7
gpofblank <metodiev.design@gmail.com>
2020-08-20 04:29:41 +0300

    list and add users logic done

--- src/app/users/create-user-page/create-user-page.component.ts
+++ src/app/users/create-user-page/create-user-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../users/models/user';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {RootState, selectUserById} from '../../root.state';
+import * as UserActions from '../../users/actions/user.actions';
+import * as TodoActions from '../../todos/actions/todo.actions';
 
 @Component({
   selector: 'app-create-user-page',
@@ -6,10 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user-page.component.css']
 })
 export class CreateUserPageComponent implements OnInit {
+  public user: User =
+    {
+      id: 0,
+      name: '',
+      email: '',
+      tasks: []
+    };
 
-  constructor() { }
+  public userAddForm: FormGroup;
+  public createdAt: FormControl;
+  public text: FormControl;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private store: Store<RootState>, private router: Router) {
+
+    this.userAddForm = this.fb.group({
+      name: this.user.name,
+      email: this.user.email,
+      tasks: this.user.tasks,
+    });
   }
 
+  ngOnInit() {
+  }
+
+  submit() {
+    if (this.userAddForm.valid) {
+      const id: number = Math.random();
+      const userPartial: User = this.userAddForm.value;
+      const user: User = {id, ...userPartial};
+
+      this.store.dispatch(UserActions.AddUser({user}));
+
+      this.router.navigateByUrl('/users');
+    }
+  }
 }
